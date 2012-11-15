a31ccff28e29ff137ef8f290ad24765c28509ab3
Marco V. Aguilar Soto <aguilarsoto@gmail.com>
2012-11-15 15:18:58 -0300

    allow advertisers with the same name on different publishers, also remove validation on deal for end_date if deal is open

--- test/unit/advertiser_test.rb
+++ test/unit/advertiser_test.rb
@@ -2,13 +2,16 @@ require "test_helper"
 
 class AdvertiserTest < ActiveSupport::TestCase
   test "constraints" do
-    FactoryGirl.create(:advertiser, :name => "Burgerville")
-
     publisher = FactoryGirl.create(:publisher)
+    publisher.advertisers.create(:name => "Burgerville")
     advertiser = publisher.advertisers.build(:name => "Burgerville")
     assert !advertiser.valid?, "Should not allow duplicate names"
 
     advertiser.name = "In N' Out Burger"
-    assert advertiser.valid?, "Should allow multiple advertisers with unique names"
+    assert advertiser.valid?, "Should allow multiple advertisers with unique names on the same publisher"
+    
+    another_publisher = FactoryGirl.create(:publisher)
+    another_advertiser = another_publisher.advertisers.build(:name => "Burgerville")
+    assert advertiser.valid?, "Should allow multiple advertisers with the same name on the different publisher"
   end
 end
